87ae4df759c51470761171234c64d5c1005f53a8
praveen0622 <110181278+praveen0622@users.noreply.github.com>
2022-08-01 15:05:29 -0400

    Update pmotionclient.py

--- clientcodes/pmotionclient.py
+++ clientcodes/pmotionclient.py
@@ -4,11 +4,13 @@ import threading
 import socket
 from threading import *
 import cv2, pickle, struct
+#the socketing part with robot IP and PORT
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 7272
 host = '192.168.1.31'
 client_socket.connect((host, port))
 
+#the function for web cam streaming 
 def video_from_server():
     print("video stream started")
     data = b""
@@ -21,21 +23,23 @@ def video_from_server():
         packed_msg_size = data[:payload_size]
         data = data[payload_size:]
         msg_size = struct.unpack("Q", packed_msg_size)[0]
-
+	#the video is converted to a pixel stream and sent through the socket
         while len(data) < msg_size:
             data += client_socket.recv(4 * 1024)
         frame_data = data[:msg_size]
         data = data[msg_size:]
         frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
         cv2.imshow("RECEIVING VIDEO", frame)
+	#cv2 (computer vision) to extract the video from web cam
         key = cv2.waitKey(1) & 0xFF
         if key == keyboard.Key.esc:
             cv2.destroyAllWindows()
             break
     print("video stream ended")
+#thread is created for te web cam streaming process
 t1 = threading.Thread(target=video_from_server)
 t1.start()
-
+#this ensures the code can wait for key input and also stream the video in seprate thread
 from pynput import keyboard
 
 # define constants :
@@ -50,7 +54,9 @@ print("Waiting for key press \n")
 print("W,A,S,D = move robot \n")
 print("Esc = terminate \n")
 
+#function definition for key stroke input 
 class MyException(Exception): pass
+#the function is automatically called when a key is pressed 
 def on_press(key):
 	if (key == keyboard.Key.up):
 		command=str(KEY_UP)
